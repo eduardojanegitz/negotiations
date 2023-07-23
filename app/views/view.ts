fa43ab7a84a60b180c377d895f82899af7be43cc
Eduardo <edu0alves00@gmail.com>
2023-07-23 03:40:02 -0300

    protected template

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,15 +1,19 @@
 export abstract class View<T> {
   // Implementando o abstract para ela não ser instanciada
   protected elemento: HTMLElement; // Foi utilizado o modificador protected, para que os filhos possam utilizar o atributo
+  private escapar: Boolean;
 
-  constructor(seletor: string) {
+  constructor(seletor: string, escapar: boolean) {
     this.elemento = document.querySelector(seletor);
   }
 
   protected abstract template(model: T): string;
 
   public update(model: T): void {
-    const template = this.template(model);
+    let template = this.template(model);
+    if(this.escapar) {
+      template = template.replace(/<script>[\s\S]*?<\/script>/, '')
+    }
     this.elemento.innerHTML = template;
   }
 }
