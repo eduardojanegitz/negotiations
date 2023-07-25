1d242b5eb8a977a5570105e1e0bf30338d427b98
Eduardo <edu0alves00@gmail.com>
2023-07-25 20:52:32 -0300

    improving performance

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -14,8 +14,6 @@ export abstract class View<T> {
 
   protected abstract template(model: T): string;
 
-  @logarTempoExecucao()
-  @inspect()
   public update(model: T): void {
     let template = this.template(model);
     this.elemento.innerHTML = template;
